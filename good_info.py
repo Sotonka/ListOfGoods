1f06e848326ee60b08399f11548542413d511f57
Eugeny Alekseenko <puncong@mail.ru>
2021-01-11 13:32:57 +1000

    logger added

--- good_info.py
+++ good_info.py
@@ -1,4 +1,7 @@
 from datetime import date, datetime, timedelta
+import logging
+
+module_logger = logging.getLogger("loggerApp.GoodInfoList")
 
 
 class GoodInfo:
@@ -49,6 +52,8 @@ class GoodInfo:
         :param key: str
         :return: class GoodInfo
         """
+        logger = logging.getLogger("loggerApp.GoodInfo.__getitem__")
+
         if key == 0 or key == 'name':
             return self.name
         elif key == 1 or key == 'price':
@@ -60,7 +65,8 @@ class GoodInfo:
         elif key == 4 or key == 'expiration_date':
             return self.expiration_date
         else:
-            print('Такого параметра нет - {}'.format(key))
+            logger.error('key does not exist - {}'.format(key))
+            raise IndexError('{}'.format(key))
 
     @staticmethod
     def convert_to_date(date_str):
@@ -69,6 +75,8 @@ class GoodInfo:
         :param date_str: str
         :return: date (or bool if wrong type)
         """
+        logger = logging.getLogger("loggerApp.GoodInfo.convert_to_date")
+
         date_str = date_str.split('-')
         if date_str[0].isdigit() and \
                 date_str[1].isdigit() and \
@@ -78,6 +86,7 @@ class GoodInfo:
                         year=int(date_str[0]))
         else:
             print('НЕВЕРНАЯ ДАТА')
+            logger.error('wrong date - {}'.format(date_str))
             return False
 
 
@@ -122,8 +131,11 @@ class GoodInfoList:
         :param key: str
         :return: class GoodInfo or None
         """
+        logger = logging.getLogger("loggerApp.GoodInfoList.__getitem__")
+
         if isinstance(key, int):
             if key >= len(self.good_info_list):
+                logger.error('key does not exist - {}'.format(key))
                 raise IndexError('{}'.format(key))
             else:
                 return self.good_info_list[key]
@@ -141,6 +153,8 @@ class GoodInfoList:
         Создает GoodInfoList из файла записей формата str:int:int:str:int,
         :param file: str
         """
+        logger = logging.getLogger("loggerApp.GoodInfoList.read_from_file")
+
         with open(file, 'r', encoding="utf-8") as data_file:
             data = data_file.readlines()
 
@@ -149,15 +163,16 @@ class GoodInfoList:
 
             if any(map(lambda good_info:
                        good_info[0] == items[0], self.good_info_list)):
-                print(items[0])
-                print("Такой товар уже есть!")
+                logger.error('already exists {}'.format(items))
+                print("Такой товар уже есть! {}".format(items))
             else:
 
                 if len(items) == 5 and items[1].isdigit() \
                         and items[2].isdigit() \
                         and GoodInfo.convert_to_date(items[3]) \
                         and items[4].replace("\n", "").isdigit() \
-                        and GoodInfo\
+                        and int(items[4].replace("\n", "")) > 0 \
+                        and GoodInfo \
                         .convert_to_date(items[3]) >= datetime.now().date():
 
                     self.add(GoodInfo(items[0],
@@ -165,21 +180,30 @@ class GoodInfoList:
                                       int(items[2]),
                                       items[3],
                                       int(items[4].replace("\n", ""))))
+
+                    logger.info('added {}'.format(items))
+
                 else:
                     print('неверный формат {}'.format(items))
+                    logger.error('wrong format {}'.format(items))
 
     def add(self, other):
         """
         Добавляет элемент класса GoodInfo в список, если такого товара еще нет
         :param other: class GoodInfo
         """
+        logger = logging.getLogger("loggerApp.GoodInfoList.add")
+
         if isinstance(other, GoodInfo):
             if any(map(lambda good_info:
                        good_info[0] == other[0], self.good_info_list)):
+                logger.error('already exists {}'.format(other))
                 print("Такой товар уже есть!")
             else:
+                # logger.info('added {}'.format(other))
                 self.good_info_list.append(other)
         else:
+            logger.error('cant add {}'.format(other))
             raise ValueError('{}'.format(other))
 
     def remove(self, name):
@@ -187,8 +211,11 @@ class GoodInfoList:
         Удаляет из GoodInfoList объект GoodInfo с указанныи именем
         :param name: str
         """
+        logger = logging.getLogger("loggerApp.GoodInfoList.remove")
+
         for good_info in self.good_info_list:
             if good_info.name == name:
+                logger.info('removed {}'.format(good_info))
                 self.good_info_list.remove(good_info)
 
     def remove_last(self):
@@ -196,20 +223,27 @@ class GoodInfoList:
         Удаляет из GoodInfoList последний объект GoodInfo и возвращает его
         :return: class GoodInfo
         """
-        last = self.good_info_list[-1]
-        del self.good_info_list[-1]
-        return last
+        logger = logging.getLogger("loggerApp.GoodInfoList.remove_last")
+
+        if len(self.good_info_list) > 0:
+            last = self.good_info_list[-1]
+            del self.good_info_list[-1]
+            logger.info('removed {}'.format(last))
+            return last
 
     def remove_expensive(self):
         """
         При помощи метода .get_max_cost находит максимальную цену товара
         и удаляет из GoodInfoList все GoodInfo с такой ценой
         """
+        logger = logging.getLogger("loggerApp.GoodInfoList.remove_expensive")
+
         max_cost = self.get_max_cost().split(':')[1].split(',')[0] \
             .replace("руб", "").replace(" ", "")
 
         for good_info in self.good_info_list:
             if good_info.price == int(max_cost):
+                logger.info('removed {}'.format(good_info))
                 self.good_info_list.remove(good_info)
 
     def delete_expired(self):
@@ -217,11 +251,23 @@ class GoodInfoList:
         Удалает все просроченные товары и возвращает их
         :return: class GoodInfoList
         """
+        logger = logging.getLogger("loggerApp.GoodInfoList.delete_expired")
+
         expired_list = GoodInfoList()
         for good in self.good_info_list:
+            print(good)
+
+            print(good[3])
+            print(timedelta(days=good[4]))
+            print(good[3] + timedelta(days=good[4]))
+            print(good[3] + timedelta(days=good[4]) < datetime.now().date())
+
             if good[3] + timedelta(days=good[4]) < datetime.now().date():
                 expired_list.add(good)
+                print('ЭКСПАИРЕД')
+                print(good)
                 self.remove(good[0])
+        logger.info('expired removed {}'.format(expired_list))
         return expired_list
 
     def get_max_cost(self):
@@ -229,6 +275,7 @@ class GoodInfoList:
         Возвращает список товаров с макс стоимостью
         :return: list of GoodInfo
         """
+        logger = logging.getLogger("loggerApp.GoodInfoList.get_max_cost")
         max_cost = 0
         max_cost_list = []
 
@@ -240,6 +287,7 @@ class GoodInfoList:
             if good_info.price == max_cost:
                 max_cost_list.append(str(good_info))
 
+        logger.info("get_max_cost DONE")
         return '\n' + '\n'.join(max_cost_list)
 
     def get_min_cost(self):
@@ -247,6 +295,8 @@ class GoodInfoList:
         Возвращает список товаров с мин стоимостью
         :return: list of GoodInfo
         """
+        logger = logging.getLogger("loggerApp.GoodInfoList.get_min_cost")
+
         min_cost = 99999
         min_cost_list = []
 
@@ -258,6 +308,7 @@ class GoodInfoList:
             if good_info.price == min_cost:
                 min_cost_list.append(str(good_info))
 
+        logger.info("get_min_cost DONE")
         return '\n' + '\n'.join(min_cost_list)
 
     def get_min_quantity(self):
@@ -265,6 +316,8 @@ class GoodInfoList:
         Возвращает список товаров с мин количеством
         :return: list of GoodInfo
         """
+        logger = logging.getLogger("loggerApp.GoodInfoList.get_min_quantity")
+
         min_quantity = 99999
         min_quantity_list = []
 
@@ -276,6 +329,7 @@ class GoodInfoList:
             if good_info.quantity == min_quantity:
                 min_quantity_list.append(str(good_info))
 
+        logger.info("get_min_quantity DONE")
         return '\n' + '\n'.join(min_quantity_list)
 
     def __bubble_sort(self, param=0):
@@ -300,27 +354,38 @@ class GoodInfoList:
         функцию сортировки по указанному значению
         :param key: str
         """
+        logger = logging.getLogger("loggerApp.GoodInfoList.sort")
+
         if key == 'name':
             self.__bubble_sort(0)
+            logger.info("sorted by name")
         elif key == 'count':
             self.__bubble_sort(2)
+            logger.info("sorted by count")
         elif key == 'cost':
             self.__bubble_sort(1)
-        elif key == 'delivery_date':
+            logger.info("sorted by cost")
+        elif key == '':
             self.__bubble_sort(3)
+            logger.info("sorted by delivery date")
         elif key == 'expiration_date':
             self.__bubble_sort(4)
+            logger.info("sorted by expiration date")
         else:
-            raise AttributeError('{}'.format(key))
+            logger.error("wrong sorting key {}".format(key))
+            print('Невозможно отсортировать по значению {}'.format(key))
 
     def get_quantity(self):
         """
         Получает общее количество всех товаров
         :return: int
         """
+        logger = logging.getLogger("loggerApp.GoodInfoList.get_quantity")
+
         count = 0
         for good in self.good_info_list:
             count += good[2]
+        logger.info("get_quantity DONE")
         return count
 
     def get_average_price(self):
@@ -328,19 +393,32 @@ class GoodInfoList:
         Получает среднюю цену по всем товарам
         :return: float
         """
+        logger = logging.getLogger("loggerApp.GoodInfoList.get_average_price")
+
         count = 0
         for good in self.good_info_list:
             count += good[1]
-        return count / len(self.good_info_list)
+        if len(self.good_info_list) > 0:
+            logger.info("get_average_price DONE")
+            return count / len(self.good_info_list)
+        else:
+            logger.info("get_average_price DONE")
+            return 0
 
     def get_std(self):
         """
         Получает среднее отклонение для всех цен товаров
         :return: float
         """
+        logger = logging.getLogger("loggerApp.GoodInfoList.get_std")
+
         average = self.get_average_price()
         summ = 0
         for good in self.good_info_list:
             summ += (good.price - average) ** 2
-        std = (summ / len(self.good_info_list)) ** .5
-        return std
+        if len(self.good_info_list) > 0:
+            logger.info("get_std DONE")
+            return (summ / len(self.good_info_list)) ** .5
+        else:
+            logger.info("get_std DONE")
+            return 0
--- reporter.py
+++ reporter.py
@@ -1,29 +1,67 @@
 from good_info import GoodInfo, GoodInfoList
+import logging
+import logging.config
 
-DATA_PATH = 'data/goods2.info'
-goods_list = GoodInfoList()
-goods_list.read_from_file(DATA_PATH)
 
-max_cost_goods = goods_list.get_max_cost()
-min_cost_goods = goods_list.get_min_cost()
-min_quantity_goods = goods_list.get_min_quantity()
-total_quantity = goods_list.get_quantity()
-average_price = goods_list.get_average_price()
+def main():
+    # ЛОГГЕР
+    dictLogConfig = {
+        "version": 1,
+        "handlers": {
+            "fileHandler": {
+                "class": "logging.FileHandler",
+                "formatter": "myFormatter",
+                "filename": "logs/logs.log",
+                "encoding": "utf-8"
+            }
+        },
+        "loggers": {
+            "loggerApp": {
+                "handlers": ["fileHandler"],
+                "level": "INFO",
+            }
+        },
+        "formatters": {
+            "myFormatter": {
+                "format": "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+            }
+        }
+    }
 
-print("Общее количество товаров: {} \n".format(total_quantity))
-print("Средняя цена товара: {} \n".format(average_price))
-print("Самые дорогие товары: {} \n".format(max_cost_goods))
-print("Самые дешевые товары: {} \n".format(min_cost_goods))
-print("Заканчивается товар: {} \n".format(min_quantity_goods))
+    logging.config.dictConfig(dictLogConfig)
+    logger = logging.getLogger("loggerApp")
 
-print(goods_list)
-# __len__() для GoodInfolist
-print(len(goods_list))
-# get_std() получает среднее отклонение для всех цен товаров
-print(goods_list.get_std())
-# remove_last() удаляет последний товар
-print(goods_list.remove_last())
+    logger.info("Program started")
 
-print(goods_list)
+    # ПРОГРАММА
+    DATA_PATH = 'data/goods2.info'
+    goods_list = GoodInfoList()
+    goods_list.read_from_file(DATA_PATH)
 
-print(goods_list['соль 1 кг'])
+    max_cost_goods = goods_list.get_max_cost()
+    min_cost_goods = goods_list.get_min_cost()
+    min_quantity_goods = goods_list.get_min_quantity()
+    total_quantity = goods_list.get_quantity()
+    average_price = goods_list.get_average_price()
+
+    print("Общее количество товаров: {} \n".format(total_quantity))
+    print("Средняя цена товара: {} \n".format(average_price))
+    print("Самые дорогие товары: {} \n".format(max_cost_goods))
+    print("Самые дешевые товары: {} \n".format(min_cost_goods))
+    print("Заканчивается товар: {} \n".format(min_quantity_goods))
+
+    print(goods_list)
+    # __len__() для GoodInfolist
+    print(len(goods_list))
+    # get_std() получает среднее отклонение для всех цен товаров
+    print(goods_list.get_std())
+    # remove_last() удаляет последний товар
+    print(goods_list.remove_last())
+
+    print(goods_list)
+
+    print(goods_list['соль 1 кг'])
+
+
+if __name__ == "__main__":
+    main()
